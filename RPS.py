fed073bc0760c70a15f0972c8ffd1b0ac4d2a6de
olezahl <102021704+olezahl@users.noreply.github.com>
2022-06-16 12:51:41 +0200

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,8 +1,8 @@
 import random
 
 def play():
-    user = input("'r' for rock, 'p' for paper, 's' for scissors")
-    computer = random.choice(['r', 'p', 's'])
+    user = input("Choose your move wisely (roc pap sci spo liz): ")
+    computer = random.choice(['liz'])
 
     if user == computer:
         return "Tie"
@@ -10,8 +10,16 @@ def play():
     if is_win(user, computer):
         return "You won!"
     
-    return "You lost"
+    if is_loss(user, computer):
+       return "You lost!"
+
 
 def is_win(player, opponent):
-    if (player == 'r' and opponent == 's') or (player == 'p' and opponent == 'r') or (player == 's' and opponent == 'p'): 
+    if (player == 'roc' or 'spo' and opponent == 'sci') or (player == 'liz' or 'sci' and opponent == 'pap') or (player == 'spo' or 'pap' and opponent == 'roc') or (player == 'roc' or 'sci' and opponent == 'liz') or (player == 'pap' or 'liz' and opponent == 'spo'):
         return True
+
+def is_loss(player, opponent):
+   if (opponent == 'roc' or 'spo' and player == 'sci') or (opponent == 'liz' or 'sci' and player == 'pap') or (opponent == 'spo' or 'pap' and player == 'roc') or (opponent == 'roc' or 'sci' and player == 'liz') or (opponent == 'pap' or 'liz' and player == 'spo'):
+       return True
+
+print(play())
