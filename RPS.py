74bf958974e1ee69959af54165b7017416c6d530
olezahl <102021704+olezahl@users.noreply.github.com>
2022-06-16 20:27:32 +0200

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -6,20 +6,18 @@ def play():
 
     if user == computer:
         return "Tie"
-    
     if is_win(user, computer):
         return "You won!"
-    
-    if is_loss(user, computer):
-       return "You lost!"
-
+#    if is_loss(user, computer):
+#        return "You lost!"
+    return "You lost!"
 
 def is_win(player, opponent):
     if (player == 'roc' or 'spo' and opponent == 'sci') or (player == 'liz' or 'sci' and opponent == 'pap') or (player == 'spo' or 'pap' and opponent == 'roc') or (player == 'roc' or 'sci' and opponent == 'liz') or (player == 'pap' or 'liz' and opponent == 'spo'):
         return True
 
-def is_loss(player, opponent):
-   if (opponent == 'roc' or 'spo' and player == 'sci') or (opponent == 'liz' or 'sci' and player == 'pap') or (opponent == 'spo' or 'pap' and player == 'roc') or (opponent == 'roc' or 'sci' and player == 'liz') or (opponent == 'pap' or 'liz' and player == 'spo'):
-       return True
+#def is_loss(opponent, player):
+#    if (opponent == 'roc' or 'spo' and player == 'sci') or (opponent == 'liz' or 'sci' and player == 'pap') or (opponent == 'spo' or 'pap' and player == 'roc') or (opponent == 'roc' or 'sci' and player == 'liz') or (opponent == 'pap' or 'liz' and player == 'spo'):
+#       return False
 
 print(play())
