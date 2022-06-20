37b5f967600a9d83676d4c79129ce0a30dc3d21e
olezahl <102021704+olezahl@users.noreply.github.com>
2022-06-20 12:22:28 +0200

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -2,7 +2,7 @@ import random
 
 def play():
     user = input("Choose your move wisely (roc pap sci spo liz): ")
-    computer = random.choice(['liz'])
+    computer = random.choice(['roc'])
 
     if user == computer:
         return "Tie"
@@ -13,11 +13,15 @@ def play():
     return "You lost!"
 
 def is_win(player, opponent):
-    if (player == 'roc' or 'spo' and opponent == 'sci') or (player == 'liz' or 'sci' and opponent == 'pap') or (player == 'spo' or 'pap' and opponent == 'roc') or (player == 'roc' or 'sci' and opponent == 'liz') or (player == 'pap' or 'liz' and opponent == 'spo'):
+    if ((       player == 'roc' or player == 'spo' and opponent == 'sci')
+            or (player == 'liz' or player == 'sci' and opponent == 'pap')
+            or (player == 'spo' or player == 'pap' and opponent == 'roc')
+            or (player == 'roc' or player == 'sci' and opponent == 'liz')
+            or (player == 'pap' or player == 'liz' and opponent == 'spo')):
         return True
 
 #def is_loss(opponent, player):
 #    if (opponent == 'roc' or 'spo' and player == 'sci') or (opponent == 'liz' or 'sci' and player == 'pap') or (opponent == 'spo' or 'pap' and player == 'roc') or (opponent == 'roc' or 'sci' and player == 'liz') or (opponent == 'pap' or 'liz' and player == 'spo'):
-#       return False
+#       return True
 
 print(play())
